ad72323b40348360f9e64c18e515b60a2f74f11b
jesper.junker <jesper.junker@hbt.de>
2024-12-05 09:30:03 +0100

    new part2 which does both in one

--- 5/part2.py
+++ 5/part2.py
@@ -1,17 +1,18 @@
 from functools import cmp_to_key
 rules = []
 cmp = (lambda x,y: 1-2*((x, y) in rules))
+p = [0, 0]
 def parseLine(text):
     if text == '':
-        return 0
+        return
     rule = text.split('|')
     if (len(rule) > 1):
         rules.append((int(rule[0]), int(rule[1])))
     else:
         l = [int(x.strip()) for x in text.split(',')]
         s = sorted(l, key = cmp_to_key(cmp))
-        if s != l:
-            return s[(len(s) - 1)//2]
-    return 0
+        p[s!=l] += s[(len(s) - 1)//2]
 
-print(sum(parseLine(line.strip()) for line in open("5_1.txt", "r")))
+for line in open("5_1.txt", "r"):
+    parseLine(line.strip())
+print(p)
