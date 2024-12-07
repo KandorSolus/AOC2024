52b1eb54983cb002dc9bc5bfcefe5ac689baf307
jesper.junker <jesper.junker@hbt.de>
2024-12-08 00:47:56 +0100

    only add if le than res

--- 7/day7.py
+++ 7/day7.py
@@ -1,8 +1,8 @@
 f = open("7_1.txt", "r")
-def addOn(num, l):
-    a = [x+num for x in l]
-    b = [x*num for x in l]
-    c = [int(str(x) + str(num)) for x in l]
+def addOn(num, l, res):
+    a = [x+num for x in l if x+num <= res]
+    b = [x*num for x in l if x*num <= res]
+    c = [int(str(x) + str(num)) for x in l if int(str(x) + str(num)) <= res]
     return a + b + c
 def parseLine(text):
     print(text)
@@ -12,6 +12,6 @@ def parseLine(text):
     print(res, p)
     res2 = [p[0]]
     for i in range(len(p) - 1):
-        res2 = addOn(p[i + 1], res2)
+        res2 = addOn(p[i + 1], res2, res)
     return res if res in res2 else 0
 print(sum(parseLine(line.strip()) for line in f.readlines()))
